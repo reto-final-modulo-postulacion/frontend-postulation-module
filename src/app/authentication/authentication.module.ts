31e2a5ca93d1cabad6d18489a34e18e36f0ae561
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2022-08-08 09:25:48 -0500

    add contaner reponsive for the cards Trainig league

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -6,10 +6,10 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AuthRoutingModule } from "./auth-routing.module";
 import { environment } from "../../environments/environment";
 
-import { SinupComponent } from './sinup/sinup.component';
-import { LoginComponent } from './login/login.component';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { ActionPasswordComponent } from './action-password/action-password.component';
+import { SinupComponent } from "./sinup/sinup.component";
+import { LoginComponent } from "./login/login.component";
+import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
+import { ActionPasswordComponent } from "./action-password/action-password.component";
 
 @NgModule({
   declarations: [
