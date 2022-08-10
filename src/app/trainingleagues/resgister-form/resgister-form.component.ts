3bfcebb95db76d1e09a30341d78ab2b4ecd6af80
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2022-08-10 13:18:57 -0500

    We added the obtaining of countries, states, cities from the api, in a static way.

--- src/app/trainingleagues/resgister-form/resgister-form.component.ts
+++ src/app/trainingleagues/resgister-form/resgister-form.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 import { PostulantApiService } from '../service/postulant-api/postulant-api.service';
 import { Countrie } from '../interfaces/countries';
 import { ServiceApiCountriesService } from '../service/service-api-countries/service-api-countries.service';
+import { Postulant } from '../interfaces/postulant';
 
 @Component({
   selector: 'app-resgister-form',
@@ -10,8 +11,50 @@ import { ServiceApiCountriesService } from '../service/service-api-countries/ser
   styleUrls: ['./resgister-form.component.css']
 })
 export class ResgisterFormComponent implements OnInit {
-  postulant: any;
+  postulant: Postulant={
+  		"id": "",
+		"fullName": {
+			name: "",
+			lastname: ""
+		},
+		"documentUser": {
+			type: "",
+			value: ""
+		},
+		"dateOfBirth": "",
+		"nationality": "",
+		"urlPhoto": "",
+		"phone": {
+			"phoneCode": "",
+			"phoneNumber": ""
+		},
+		"email": "",
+		"companyName": "",
+		"workExperience": "",
+		"currentOccupation": "",
+		"educationalLevel": "",
+		"country": "",
+		"department": "",
+		"municipality": "",
+		"address": "",
+		"englishLevel": "",
+		"isStudying": "",
+		"aboutYou": "",
+		"urlCV": "",
+		"linkedin": "",
+		"sessionOn": true,
+		"challenge": {
+			"idChallenge": "",
+			"registrationDate": "",
+			"initialDate": "",
+			"finalDate": "",
+			"language": ""
+		},
+		"idTraining": ""
+  }
   listNameCountries: any[] = [];
+  listNameStates: any[] = [];
+  listNameCities: any[] = [];
 
   formRegisterLigue = new FormGroup({
     name: new FormControl(''),
@@ -25,9 +68,10 @@ export class ResgisterFormComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-  
     this.obtenerPaises();
     this.getPostulantById();
+    this.getAllStatesOfCountry();
+    this.getAllCitiesOfCountry();
   }
 
   getPostulantById() {
@@ -45,19 +89,45 @@ export class ResgisterFormComponent implements OnInit {
     // }
   }
 
-
-
-  obtenerPaises() {
-    let token = JSON.parse(localStorage.getItem("token")!);
+  async obtenerPaises() {
+    let token = await JSON.parse(localStorage.getItem("token")!);
     this.countriesApiService
       .getAllCountries(token)
-      .subscribe((listCountries: any) => {      
+      .subscribe((listCountries: any) => {
         this.listNameCountries = listCountries.map((auxCountries: any) => {
           return {
-            name: auxCountries.country_name,
+            name: auxCountries.country_name
           }
         });
-      });      
-      console.log(this.listNameCountries)
+      });
   }
+
+  getAllStatesOfCountry(){
+  let token = JSON.parse(localStorage.getItem("token")!);
+    this.countriesApiService
+      .getAllStatesOfCountry(token,"Colombia")
+      .subscribe((listStates: any) => {
+        this.listNameStates = listStates.map((auxStates: any) => {
+          return {
+            name: auxStates.state_name
+          }
+        });
+      });
+  }
+
+  getAllCitiesOfCountry(){
+  let token = JSON.parse(localStorage.getItem("token")!);
+    this.countriesApiService
+      .getAllCitiesOfState(token,"Antioquia")
+      .subscribe((listCities: any) => {
+        this.listNameCities = listCities.map((auxCities: any) => {
+          return {
+            name: auxCities.city_name
+          }
+        });
+          console.log(listCities);
+      });
+  }
+
+
 }
